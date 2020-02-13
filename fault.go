ebd79382ecee12b2ae09e680790088aa9ad02065
K Sudheernath Reddy <ksudheernathreddy@gmail.com>
2020-02-13 15:51:13 +0530

    time before response

--- fault.go
+++ fault.go
@@ -1,15 +1,19 @@
 package main
 
 import(
-	"net/http"
-	"log"
-	"fmt"
-)
+        "net/http"
+        "log"
+        "fmt"
+        "time"
+        "os"
+        )
 
 func main() {
-    fmt.Println("Starting rest server...")
-    http.HandleFunc("/reviews/0", func(w http.ResponseWriter, r *http.Request) {
-	http.Error(w, "dummy", 502)
-    })
-    log.Fatal(http.ListenAndServe(":9080", nil))
+dur, _ := time.ParseDuration(os.Getenv("SLEEP_DURATION"))
+fmt.Printf("Starting rest server with %v...", dur)
+http.HandleFunc("/reviews/0", func(w http.ResponseWriter, r *http.Request) {
+ time.Sleep(dur)
+ http.Error(w, "dummy", 502)
+})
+log.Fatal(http.ListenAndServe(":9080", nil))
 }
